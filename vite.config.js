44ff20fd918eb0620650f67b088f876114a9712b
ie-nida <nida.nidashahzad@gmail.com>
2025-06-01 16:28:32 +0400

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { Agent } from 'https';
-
-// ⚠️ This import will fail unless you created this plugin manually
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -12,11 +9,13 @@ export default defineConfig({
       '/osu-api': {
         target: 'https://osu.ppy.sh',
         changeOrigin: true,
-        secure: false,
-        agent: new Agent({ keepAlive: true }),
+        secure: true,
         rewrite: (path) => path.replace(/^\/osu-api/, ''),
       },
+      '/api': {
+        target: 'http://localhost:10000',
+        changeOrigin: true,
+      }
     },
   },
 });
-
